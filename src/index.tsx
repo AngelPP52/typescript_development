731e559f7514516afbf9a9d2f34a3e11f743d184
Angel屁屁 <1017118152@qq.com>
2020-11-20 00:19:13 +0800

    feat: init

--- src/index.tsx
+++ src/index.tsx
@@ -6,4 +6,4 @@ const root = document.getElementById("root");
 const props = {className: "title"};
 const element = React.createElement("div", props, "hello");
 
-ReactDom.render(element, root);
+ReactDom.render(element, root); 
